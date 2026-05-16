9f30e29124570562bf568e9cfa2185e7b2608f3a
Huangchu8year <1280605442@qq.com>
2026-05-16 18:40:18 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,11 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: '/portfolio/',
+   export default defineConfig({
+  base: '/test/',
+  plugins: [react(), tailwindcss()],
+  // ...其他配置
+})
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
